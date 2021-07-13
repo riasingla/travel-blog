1d6c9956edfb0b64269f5e4d3c00498a08f0d310
Ria singla <riasingla28@gmail.com>
2021-07-13 21:46:58 +0530

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+    document.getElementById("Contact-link").onclick = function () {
+        location.href = "contact.html";
+    };
+
+    document.getElementById("About-link").onclick = function () {
+        location.href = "About.html";
+    }
+      
+    document.getElementById("Home-link").onclick = function () {
+        location.href = "Home.html";
+    }
+      
+    document.getElementById("Blogs-link").onclick = function () {
+        location.href = "blog.html";
+    }
+      
